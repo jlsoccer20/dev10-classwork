3bc886a055b37c1042c368f32c657d8118e7c355
Jessica Link <jlsoccer20@users.noreply.github.com>
2023-08-07 23:15:28 +0200

    update exercises 2-3

--- week-01/exercises/arrays-exercises/src/Exercise02.java
+++ week-01/exercises/arrays-exercises/src/Exercise02.java
@@ -4,9 +4,20 @@ public class Exercise02 {
 
         String[] tenFavoriteFoods = new String[10]; // space for 10 favorite foods
 
-        tenFavoriteFoods[5] = "squid ink";
-        System.out.println(tenFavoriteFoods[5]);
-        System.out.println(tenFavoriteFoods[6]);
+        tenFavoriteFoods[0] = "sukiyaki"; // top choice
+        tenFavoriteFoods[1] = "takoyaki"; //second
+        tenFavoriteFoods[2] = "okonomiyaki"; //third
+        tenFavoriteFoods[3] = "sushi"; //fourth
+        tenFavoriteFoods[4] = "gimbap"; //fifth
+        tenFavoriteFoods[5] = "japchae"; //sixth
+        tenFavoriteFoods[6] = "pizza"; //7th
+        tenFavoriteFoods[7] = "ramen"; //8th
+        tenFavoriteFoods[8] = "pasta"; // 9th
+        tenFavoriteFoods[9] = "ice cream"; //last
+
+        System.out.println(tenFavoriteFoods[0]); //top
+        System.out.println(tenFavoriteFoods[5]); // sixth
+        System.out.println(tenFavoriteFoods[9]); // last
 
         // 1. Set your 10 favorite foods. (It's okay to replace squid ink.)
         // 2. Print your top, 6th, and last favorite from tenFavoriteFoods.
