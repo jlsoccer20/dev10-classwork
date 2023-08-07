fa033299fe296515d2e0c15c5be818f9da0a1630
Jessica Link <jlsoccer20@users.noreply.github.com>
2023-08-07 23:11:48 +0200

    a commit message

--- week-01/exercises/arrays-exercises/src/Exercise01.java
+++ week-01/exercises/arrays-exercises/src/Exercise01.java
@@ -4,5 +4,8 @@ public class Exercise01 {
         String[] legendaryCreatures = {"fairies", "goblins", "gnomes", "unicorns", "harpies", "solar fish"};
         System.out.println(legendaryCreatures[2]); // third creature - gnomes
         // 1. Print the first, fourth, and sixth creature to the console.
+        System.out.println(legendaryCreatures[0]); //first - fairies
+        System.out.println(legendaryCreatures[3]); //fourth - unicorns
+        System.out.println(legendaryCreatures[5]); //sixth - solar fish
     }
 }
