d82da071f60590c6d79c24fc15b956f6b6d49bf9
Jessica Link <jlsoccer20@users.noreply.github.com>
2023-08-08 18:29:46 +0200

    Update Week 1 Day 1 Exercises 13-14 Again

--- week-01/exercises/variables-and-types-exercises/src/Exercise15.java
+++ week-01/exercises/variables-and-types-exercises/src/Exercise15.java
@@ -2,12 +2,16 @@ public class Exercise15 {
 
     public static void main(String[] args) {
 
-        int value = 24;
+        int value = 115;
 
         // 1. Declare a boolean variable inRange.
+        boolean inRange = (value >= 10 && value <= 20) || (value >= 110 && value <= 120);
         // 2. When value is between 10 and 20 OR value is between 110 and 120, inRange is true. Otherwise, it's false.
         // 3. Build an expression using value to set inRange.
+
         // 4. Print the result.
+        System.out.println("Exercise 15: value is between 10 and 20 or 110 and 120: " + inRange);
+
         // 5. Change value to test different cases:
         // - less than 10
         // - between 10 and 20
