79ba42ebf179ce4ef340b2fe6b56134eac179569
Jessica Link <jlsoccer20@users.noreply.github.com>
2023-08-13 20:29:49 +0200

    Functional baseline for Hotel. Some parts don't work yet

--- week-01/assessment/capsule-hotel/src/Hotel.java
+++ week-01/assessment/capsule-hotel/src/Hotel.java
@@ -1,17 +1,18 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Hotel {
     public static void main(String[] args) {
 
         System.out.println("Welcome to the Capsule Hotel!");
-        System.out.print("How many capsules will your hotel have? \nNumber of capsules: ");
+        System.out.print("How many capsules will your hotel have? \nPlease enter a number: ");
 
         // Use this to take user input
         Scanner console = new Scanner(System.in);
 
         String userInput = console.nextLine();
 
-        System.out.printf("Capsule Hotel now has %s rooms!\n", userInput);
+        System.out.printf("\nCapsule Hotel now has %s rooms!\n", userInput);
 
         // convert user input to integer to match variable type
         int numCapsules = Integer.parseInt(userInput);
@@ -21,35 +22,100 @@ public class Hotel {
         String[] capsules = new String[numCapsules];
 
 
+        // creating variable name outside of loop
+        //String capsuleNumberCheckOutString;
 
-        // View the room
+        // Create and loop over main menu
+        String input;
+        do {
+            System.out.println("\nMain Menu");
+            System.out.println("-----------------");
+            System.out.println("1. View Capsules");
+            System.out.println("2. Check-in a guest");
+            System.out.println("3. Check-out a guest");
+            System.out.println("4. Exit\n");
 
-        System.out.println("Which Capsule number would you like to see?\nPlease enter a number here: ");
+            System.out.print("What would you like to do now?\nSelect [1-4] : ");
+            input = console.nextLine();
 
-        String userInputCapsuleNumber = console.nextLine();
-        int desiredCapsuleNumber = Integer.parseInt(userInputCapsuleNumber);
+            // 1. View Capsules
+            if(input.equals("1")){
 
-        if (capsules[desiredCapsuleNumber] == null){
-            System.out.printf("Capsule %s is empty!\n", desiredCapsuleNumber);
+                // 1. View Capsules
 
-        } else {
-            System.out.printf("Capsule %s contains guest name: %s", desiredCapsuleNumber,capsules[desiredCapsuleNumber]);
-        }
+                // Wish to display 10 capsules...
+                displayCapsules(capsules);
 
-        // Check-in a Guest
-        System.out.println("Please enter a guest name: ");
-        String userInputGuestName = console.nextLine();
+                System.out.println("Which Capsule number would you like to see?\nPlease enter a number here: ");
 
-        System.out.println("Please enter a Capsule number to check into: ");
-        String capsuleNumberCheckInString = console.nextLine();
-        int capsuleNumberCheckIn = Integer.parseInt(capsuleNumberCheckInString);
+                String userInputCapsuleNumber = console.nextLine();
+                int desiredCapsuleNumber = Integer.parseInt(userInputCapsuleNumber);
 
-        capsules[capsuleNumberCheckIn] = userInputGuestName;
+                if (capsules[desiredCapsuleNumber] == null){
+                    System.out.printf("Capsule %s is empty!\n", desiredCapsuleNumber);
 
-        System.out.printf("%s has checked in to Capsule %s!\n", userInputGuestName, capsuleNumberCheckIn);
-        System.out.printf("Capsule %s contains guest name: %s\n", capsuleNumberCheckIn,capsules[capsuleNumberCheckIn]);
+                } else {
+                    System.out.printf("Capsule %s contains guest name: %s.\n", desiredCapsuleNumber,capsules[desiredCapsuleNumber]);
+                }
 
-        System.out.println(capsules);
+            } else if (input.equals("2")) {
+
+                // 2. Check-in a Guest
+
+                System.out.println("Please enter a guest name to check-in: ");
+                String userInputGuestName = console.nextLine();
+
+                System.out.println("Please enter a Capsule number to check into: ");
+                String capsuleNumberCheckInString = console.nextLine();
+                int capsuleNumberCheckIn = Integer.parseInt(capsuleNumberCheckInString);
+
+                capsules[capsuleNumberCheckIn] = userInputGuestName;
+
+                System.out.printf("%s has checked in to Capsule %s!\n", userInputGuestName, capsuleNumberCheckIn);
+                //System.out.printf("Capsule %s contains guest name: %s\n", capsuleNumberCheckIn,capsules[capsuleNumberCheckIn]);
+
+            } else if (input.equals("3")){
+                System.out.println("Check-out guest from capsule number: ");
+                String capsuleNumberCheckOutString = console.nextLine();
+
+                int capsuleNumberCheckOutInt = Integer.parseInt(capsuleNumberCheckOutString);
+
+
+                // Validate answer
+                if (capsules[capsuleNumberCheckOutInt] == null){
+                    System.out.printf("Capsule %s is already empty!\n", capsuleNumberCheckOutString);
+
+                } else {
+
+                    System.out.printf("Guest has now been checked out of Capsule %s.\n", capsuleNumberCheckOutString);
+
+                    // Set capsule number to empty
+                    capsules[capsuleNumberCheckOutInt] = null;
+                    //System.out.printf("Capsule %s contains guest name: %s", desiredCapsuleNumber,capsules[desiredCapsuleNumber]);
+                }
+
+
+
+            } else if (input.equals("4")){
+
+                // Typing n does not actually prevent the game from exiting!!!
+                System.out.println("Are you sure you would like to exit? All data will be lost. [y/n]");
+                String exitCapsuleHotel = console.nextLine();
+                    if (exitCapsuleHotel.equals("y")){
+                        System.out.println("Thank you for visiting the Capsule Hotel. Goodbye!");
+                        break;
+                    }
+
+            } else {
+                System.out.println("That is not a valid command! Please pick a number between 1 and 4!");
+            }
+
+        } while (!input.equals("4"));
+
+
+
+
+        //System.out.println(capsules);
 
         // Prompt user input (int)
         // Validate input (please enter a number, banana is not a number)
@@ -125,5 +191,12 @@ public class Hotel {
     }
 
          */
+    }
+
+    static void displayCapsules(String[] capsules){
+        System.out.println("Guests");
+        for (int i = 0; i < capsules.length; i++){
+            System.out.printf("Capsule %s: %s%n", i + 1, capsules[i] == null ? "[Unoccupied]" : capsules[i]);
+        }
     }
 }
