9634b5a1c03add203914643dbc33ebb8b5a0e863
Jessica Link <jlsoccer20@users.noreply.github.com>
2023-09-15 20:19:56 +0200

    Pushing m05

--- week-05/assessment/dwmh/sql/test-schema.sql
+++ week-05/assessment/dwmh/sql/test-schema.sql
@@ -47,3 +47,38 @@ create table reservation (
         foreign key (guest_user_id)
         references user(user_id)    
 );
+
+delimiter //
+create procedure set_known_good_state()
+begin
+	-- delete starts with children	
+    delete from reservation;
+    alter table reservation auto_increment=1;
+    delete from location;
+    alter table location auto_increment=1;
+    delete from state;
+    alter table state auto_increment=1;
+	delete from `user`;
+    alter table `user` auto_increment=1;
+    
+    -- inserts start with parents
+	insert into `user` (first_name, last_name, email, phone) values
+		('First 1', 'Last 1', '1@example.com', '555-5551'),
+        ('First 2', 'Last 2', '2@example.com', '555-5552'),
+        ('First 3', 'Last 3', '3@example.com', '555-5553');
+        
+	insert into state(`name`, usps_code) values ('Minnesota', 'MN');
+    
+    -- user_id == host
+    insert into location (user_id, address, city, state_id, postal_code, standard_rate, weekend_rate)
+		values
+        (1, 'Location 1', 'City 1', 1, '55551', 100.0, 125.0),
+        (3, 'Location 2', 'City 2', 1, '55552', 100.0, 125.0);
+        
+	insert into reservation (guest_user_id, location_id, start_date, end_date, total)
+		values
+        (1, 1, '2023-01-01', '2023-01-05', 500.0),
+        (2, 1, '2023-01-05', '2023-01-07', 500.0),
+        (2, 2, '2023-01-01', '2023-01-05', 500.0);
+end//
+delimiter ;
