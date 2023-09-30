4cc3fe4913d7a7217b5d716905cfdee0b04a4d4f
Jessica Link <jlsoccer20@users.noreply.github.com>
2023-09-30 17:26:19 +0200

    Practicing npm

--- week-08/exercises/ChooseYourOwnNpmAdventure/index.js
+++ week-08/exercises/ChooseYourOwnNpmAdventure/index.js
@@ -1,1 +1,9 @@
-console.log("Hello World")
+const {addDays, format} = require('date-fns');
+
+const today = format(new Date(), 'dd.MM.yyyy');
+console.log(today);
+
+const nextWeek = new Date((addDays(today, 7)));
+console.log(nextWeek);
+
+console.log("Hello World");
